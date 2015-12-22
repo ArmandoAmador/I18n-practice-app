a43d8ec4290f58c004d88550df8ddaa26362fa2c
ArmandoAmador <arm.amador@gmail.com>
2015-12-22 14:24:51 -0500

    Add ability to change language
     - fix bug on russian yml file where it was pointing to english translation
     - Add dropdown to select language
     - add route to change locale

--- config/application.rb
+++ config/application.rb
@@ -29,7 +29,6 @@ module I18nPractice
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     I18n.available_locales = [:en, :ru]
-    config.time_zone = "Moscow"
     config.i18n.default_locale
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
