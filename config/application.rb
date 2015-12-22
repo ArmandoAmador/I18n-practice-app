3fd99243c8354178525d76bb82fef7d689925e35
ArmandoAmador <arm.amador@gmail.com>
2015-12-22 13:50:59 -0500

    internationalize application to russian translation
     - Create english and russian yml file
     - setup locale and time zone to russia

--- config/application.rb
+++ config/application.rb
@@ -28,6 +28,9 @@ module I18nPractice
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    I18n.available_locales = [:en, :ru]
+    config.time_zone = "Moscow"
+    config.i18n.default_locale
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
